f1e5a72d07eb2a7037cf3697d3fca0e2cb55f58f
evolvingkid <fayezmohammed23@gmail.com>
2021-06-11 19:17:34 +0530

    demon process

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import os, sys
+from daemonize import Daemonize
+
 from watcher import Watcher
 
 
-watcher = Watcher();
-watcher.run();
+def main():
+    watcher = Watcher();
+    watcher.run();
+
+
+if __name__ == '__main__':
+        myname=os.path.basename(sys.argv[0])
+        pidfile='/tmp/%s' % myname       # any name
+        daemon = Daemonize(app=myname,pid=pidfile, action=main)
+        daemon.start()
